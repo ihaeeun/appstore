6b21a6b90135ab3aeee87ec24f6b8dd9c3767e3c
haeeun <leehe@swu.ac.kr>
2020-07-13 20:52:58 +0900

    init

---
+++ appstore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appstore
+//
+//  Created by 이해은 on 2020/07/13.
+//  Copyright © 2020 Hailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
